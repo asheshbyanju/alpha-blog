4d49415451476c98457daea1410840a8136d2afa
asheshbyanju <byanju.ashesh@gmail.com>
2018-07-04 11:44:08 +0545

    update all views with category

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
   
   before_action :set_article, only: [:edit, :update, :show, :destroy]
-  before_action :require_user, except: [:index, :show]
+  #before_action :require_user, except: [:index, :show]
   before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def index
@@ -53,11 +53,11 @@ class ArticlesController < ApplicationController
     end
 
     def article_params
-      params.require(:article).permit(:title, :description)
+      params.require(:article).permit(:title, :description, category_ids: [])
     end
 
     def require_same_user
-      if current_user != @article_user and !current_user.admin?
+      if current_user != @article.user and !current_user.admin?
         flash[:danger] = "you can only edit or delete your own article"
         redirect_to root_path
       end
